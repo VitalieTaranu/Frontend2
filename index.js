68adb6f19fcc8b4a3430fae0300f9e73de02746b
VitalieTaranu <109237007+VitalieTaranu@users.noreply.github.com>
2022-10-21 19:35:01 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+
+// import App from "./App";
+
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.js";
+
+import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+// reportWebVitals();
